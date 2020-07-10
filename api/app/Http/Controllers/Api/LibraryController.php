52dc8071cc849ba0b409fb7f61465c4b72ad33e0
Asif Ali <shea786@live.co.uk>
2020-07-10 14:13:49 +0100

    started to add app with laravel

---
+++ api/app/Http/Controllers/Api/LibraryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Library;
+use Illuminate\Http\Request;
+
+class LibraryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Library::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Library $library)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Library $library)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Library $library)
+    {
+        //
+    }
+}
---
+++ api/database/migrations/2020_07_10_102911_create_library_folders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibraryFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('library_folders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('library_id');
+            $table->string('path');
+            $table->timestamps();
+
+            $table->foreign('library_id')->references('id')->on('libraries')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('library_folders');
+    }
+}
